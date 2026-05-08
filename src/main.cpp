4cc517e20d753ecacbaf39fe236dacfd525e15c1
nanathedev45 <nanayawoparek405@gmail.com>
2026-05-08 17:39:01 +0000

    initialising pins

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ int IN2;
 int ENB;
 int IN3;
 int IN4;
+int speed;
 
 void setup() {
     Serial.begin(115200);
@@ -19,3 +20,62 @@ void setup() {
     pinMode(IN4, OUTPUT);
 }
 
+void drive(int actualSpeed) {
+    PS4.setLed(0, 255, 0);
+    PS4.sendToController();
+    analogWrite(ENA, actualSpeed);
+    analogWrite(ENB, actualSpeed);
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+}
+
+void reverse(int actualSpeed) {
+    PS4.setLed(255, 0, 0);
+    PS4.sendToController();
+    analogWrite(ENA, actualSpeed);
+    analogWrite(ENB, actualSpeed);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+}
+
+void stop() {
+    digitalWrite(ENA, LOW);
+    digitalWrite(ENB, LOW);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+}
+
+void move(int speed, int actualSpeed) {
+    if (speed > 0) {
+        drive(actualSpeed);
+    }
+    else if (speed<0) {
+        reverse(actualSpeed);
+    }
+    else if (speed == 0) {
+        stop();
+    }
+    else {
+        stop();
+    }
+}
+
+void loop() {
+    speed = PS4.LStickY();
+    int actualSpeed = map(speed, -255, 0, 255, 1023);
+
+    if (PS4.isConnected()) {
+        Serial.print("Connected");
+        PS4.setLed(100, 25, 255);
+        PS4.sendToController();
+        move(speed, actualSpeed);
+    } else {
+        Serial.print("Fail");
+    }
+}
